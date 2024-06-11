47cf6ae505f46d20d6268f6bf4abcf21f4e077ed
uch105 <only.uch105@gmail.com>
2024-06-11 20:44:51 +0600

    wss correction

--- kamrulacademy/asgi.py
+++ kamrulacademy/asgi.py
@@ -9,18 +9,21 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 # your_project_name/asgi.py
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import ka_main.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from ka_main import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kamrulacademy.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            ka_main.routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
         )
     ),
 })
