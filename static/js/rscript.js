0fbd7e36e6e2898403015c7dd9ae97e5200f76ba
uch105 <only.uch105@gmail.com>
2024-08-21 11:14:24 +0600

    Video sequencing source problem

--- static/js/rscript.js
+++ static/js/rscript.js
@@ -20,18 +20,10 @@ var progress = document.getElementById("progress").value
 
 document.getElementById("progressbar").style.width = `${progress}%`;
 
-const video = document.querySelector(".video");
 
-function playVideo(){
-    if (video.paused || video.ended) {
-    video.play();
-    } else {
-    video.pause();
-    }
+function PlayModuleVideo(s){
+    class_name = "class_"+s+"_name"
+    class_video = "class_"+s+"_video"
+    document.getElementById("vdosrc").src = document.getElementById(class_video).innerText;
+    document.getElementById("vdoname").innerHTML = document.getElementById(class_name).innerText;
 }
-
-
-//function PlayModuleVideo(s){
-//    document.getElementById("vdosrc").src = {{ "module.class_"+s+"_video.url" }};
-//    document.getElementById("vdoname").innerHTML = {{ "module.class_"+s+"_name" }};
-//}
